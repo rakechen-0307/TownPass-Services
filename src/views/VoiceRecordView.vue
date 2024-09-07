2e643e40f081e8a6cb0fd0c0a6280e8a6f87fdde
rakechen-0307 <rakechen168@gmail.com>
2024-09-07 09:57:16 +0800

    voice record

--- src/views/VoiceRecordView.vue
+++ src/views/VoiceRecordView.vue
@@ -6,38 +6,29 @@ import BaseButton from '@/components/atoms/BaseButton.vue';
 import { useHandleConnectionData } from '@/composables/useHandleConnectionData';
 
 const router = useRouter();
-const text = ref('stop');
 const recordText = ref('');
 
 // 接收 APP 回傳資料
 const handleRecordText = (event: { data: string }) => {
     const result: { name: string; data: string } = JSON.parse(event.data);
     recordText.value = result.data;
-    console.log(result);
 }
 useHandleConnectionData(handleRecordText);
 
 // 雙向溝通發生的地方
 const onRecordClick = () => {
-    if (text.value == 'stop') {
-        text.value = 'start';
-        console.log(text.value);
-        useConnectionMessage('voice_record', text.value);
-    }
-    else if (text.value == 'start') {
-        text.value = 'stop';
-        console.log(text.value);
-        useConnectionMessage('voice_record', text.value);
-    }
+    useConnectionMessage('voice_record', 'start');
 };
 </script>
 
 <template>
     <div class="px-4 pt-10">
-        <h4>{{ recordText }}</h4>
+        <div class="flex justify-center">
+            <h4>{{ recordText }}</h4>
+        </div>
         <div class="fixed bottom-0 left-0 w-screen p-4 bg-white flex justify-center">
             <BaseButton class="w-full" @click="onRecordClick">
-                {{ text == 'start' ? "Stop Recording" : "Start Recording" }}
+                Start Recording
             </BaseButton>
         </div>
     </div>
